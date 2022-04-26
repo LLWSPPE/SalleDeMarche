ce846512c5bb500e6c39a48631461029d66e2098
Nathan SUK <46647779+nathansuk@users.noreply.github.com>
2022-04-26 12:47:31 +0200

    correectifs

--- UserInterface/Inscription/RegisterForm.Designer.cs
+++ UserInterface/Inscription/RegisterForm.Designer.cs
@@ -44,7 +44,7 @@ namespace LLWS.UserInterface
             // 
             // lblnscription
             // 
-<<<<<<< Updated upstream:UserInterface/Inscription/RegisterForm.Designer.cs
+
             this.lblnscription.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.lblnscription.AutoSize = true;
             this.lblnscription.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -55,34 +55,15 @@ namespace LLWS.UserInterface
             this.lblnscription.Size = new System.Drawing.Size(157, 38);
             this.lblnscription.TabIndex = 0;
             this.lblnscription.Text = "Inscription";
-=======
-            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold);
-            this.label1.ForeColor = System.Drawing.SystemColors.Control;
-            this.label1.Location = new System.Drawing.Point(313, 101);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(165, 25);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Inscription page";
->>>>>>> Stashed changes:UserInterface/RegisterForm.Designer.cs
             // 
             // txbFirstName
             // 
             this.txbFirstName.Anchor = System.Windows.Forms.AnchorStyles.None;
-<<<<<<< Updated upstream:UserInterface/Inscription/RegisterForm.Designer.cs
             this.txbFirstName.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txbFirstName.Location = new System.Drawing.Point(253, 220);
             this.txbFirstName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txbFirstName.Name = "txbFirstName";
             this.txbFirstName.Size = new System.Drawing.Size(279, 30);
-=======
-            this.txbFirstName.Font = new System.Drawing.Font("Segoe UI", 9.75F);
-            this.txbFirstName.Location = new System.Drawing.Point(165, 154);
-            this.txbFirstName.Margin = new System.Windows.Forms.Padding(2);
-            this.txbFirstName.Name = "txbFirstName";
-            this.txbFirstName.Size = new System.Drawing.Size(210, 25);
->>>>>>> Stashed changes:UserInterface/RegisterForm.Designer.cs
             this.txbFirstName.TabIndex = 1;
             this.txbFirstName.Text = "Nom";
             this.txbFirstName.TextChanged += new System.EventHandler(this.txbFirstName_TextChanged);
@@ -92,19 +73,11 @@ namespace LLWS.UserInterface
             // txbLastName
             // 
             this.txbLastName.Anchor = System.Windows.Forms.AnchorStyles.None;
-<<<<<<< Updated upstream:UserInterface/Inscription/RegisterForm.Designer.cs
             this.txbLastName.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txbLastName.Location = new System.Drawing.Point(553, 220);
             this.txbLastName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txbLastName.Name = "txbLastName";
             this.txbLastName.Size = new System.Drawing.Size(279, 30);
-=======
-            this.txbLastName.Font = new System.Drawing.Font("Segoe UI", 9.75F);
-            this.txbLastName.Location = new System.Drawing.Point(391, 154);
-            this.txbLastName.Margin = new System.Windows.Forms.Padding(2);
-            this.txbLastName.Name = "txbLastName";
-            this.txbLastName.Size = new System.Drawing.Size(210, 25);
->>>>>>> Stashed changes:UserInterface/RegisterForm.Designer.cs
             this.txbLastName.TabIndex = 2;
             this.txbLastName.Text = "Prénom";
             this.txbLastName.Enter += new System.EventHandler(this.txbLastName_Enter);
@@ -113,19 +86,11 @@ namespace LLWS.UserInterface
             // txbEmail
             // 
             this.txbEmail.Anchor = System.Windows.Forms.AnchorStyles.None;
-<<<<<<< Updated upstream:UserInterface/Inscription/RegisterForm.Designer.cs
             this.txbEmail.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txbEmail.Location = new System.Drawing.Point(253, 277);
             this.txbEmail.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txbEmail.Name = "txbEmail";
             this.txbEmail.Size = new System.Drawing.Size(580, 30);
-=======
-            this.txbEmail.Font = new System.Drawing.Font("Segoe UI", 9.75F);
-            this.txbEmail.Location = new System.Drawing.Point(165, 203);
-            this.txbEmail.Margin = new System.Windows.Forms.Padding(2);
-            this.txbEmail.Name = "txbEmail";
-            this.txbEmail.Size = new System.Drawing.Size(436, 25);
->>>>>>> Stashed changes:UserInterface/RegisterForm.Designer.cs
             this.txbEmail.TabIndex = 3;
             this.txbEmail.Text = "Email";
             this.txbEmail.Enter += new System.EventHandler(this.txbEmail_Enter);
@@ -134,19 +99,11 @@ namespace LLWS.UserInterface
             // txbPassword
             // 
             this.txbPassword.Anchor = System.Windows.Forms.AnchorStyles.None;
-<<<<<<< Updated upstream:UserInterface/Inscription/RegisterForm.Designer.cs
             this.txbPassword.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txbPassword.Location = new System.Drawing.Point(251, 327);
             this.txbPassword.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txbPassword.Name = "txbPassword";
             this.txbPassword.Size = new System.Drawing.Size(580, 30);
-=======
-            this.txbPassword.Font = new System.Drawing.Font("Segoe UI", 9.75F);
-            this.txbPassword.Location = new System.Drawing.Point(165, 243);
-            this.txbPassword.Margin = new System.Windows.Forms.Padding(2);
-            this.txbPassword.Name = "txbPassword";
-            this.txbPassword.Size = new System.Drawing.Size(436, 25);
->>>>>>> Stashed changes:UserInterface/RegisterForm.Designer.cs
             this.txbPassword.TabIndex = 4;
             this.txbPassword.Text = "Mot de passe";
             this.txbPassword.Enter += new System.EventHandler(this.txbPassword_Enter);
@@ -155,19 +112,11 @@ namespace LLWS.UserInterface
             // txbConfirmPassword
             // 
             this.txbConfirmPassword.Anchor = System.Windows.Forms.AnchorStyles.None;
-<<<<<<< Updated upstream:UserInterface/Inscription/RegisterForm.Designer.cs
             this.txbConfirmPassword.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txbConfirmPassword.Location = new System.Drawing.Point(253, 375);
             this.txbConfirmPassword.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txbConfirmPassword.Name = "txbConfirmPassword";
             this.txbConfirmPassword.Size = new System.Drawing.Size(580, 30);
-=======
-            this.txbConfirmPassword.Font = new System.Drawing.Font("Segoe UI", 9.75F);
-            this.txbConfirmPassword.Location = new System.Drawing.Point(165, 282);
-            this.txbConfirmPassword.Margin = new System.Windows.Forms.Padding(2);
-            this.txbConfirmPassword.Name = "txbConfirmPassword";
-            this.txbConfirmPassword.Size = new System.Drawing.Size(436, 25);
->>>>>>> Stashed changes:UserInterface/RegisterForm.Designer.cs
             this.txbConfirmPassword.TabIndex = 5;
             this.txbConfirmPassword.Text = "Confirmez le mot de passe";
             this.txbConfirmPassword.Enter += new System.EventHandler(this.txbConfirmPassword_Enter);
@@ -176,7 +125,7 @@ namespace LLWS.UserInterface
             // btnRegister
             // 
             this.btnRegister.Anchor = System.Windows.Forms.AnchorStyles.None;
-<<<<<<< Updated upstream:UserInterface/Inscription/RegisterForm.Designer.cs
+
             this.btnRegister.BackColor = System.Drawing.Color.SeaGreen;
             this.btnRegister.FlatAppearance.BorderSize = 0;
             this.btnRegister.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
@@ -186,13 +135,6 @@ namespace LLWS.UserInterface
             this.btnRegister.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnRegister.Name = "btnRegister";
             this.btnRegister.Size = new System.Drawing.Size(391, 48);
-=======
-            this.btnRegister.Font = new System.Drawing.Font("Segoe UI", 9.75F);
-            this.btnRegister.Location = new System.Drawing.Point(351, 342);
-            this.btnRegister.Margin = new System.Windows.Forms.Padding(2);
-            this.btnRegister.Name = "btnRegister";
-            this.btnRegister.Size = new System.Drawing.Size(76, 27);
->>>>>>> Stashed changes:UserInterface/RegisterForm.Designer.cs
             this.btnRegister.TabIndex = 11;
             this.btnRegister.Text = "M\'inscrire";
             this.btnRegister.UseVisualStyleBackColor = false;
@@ -201,7 +143,6 @@ namespace LLWS.UserInterface
             // btnReturnToLogin
             // 
             this.btnReturnToLogin.Anchor = System.Windows.Forms.AnchorStyles.None;
-<<<<<<< Updated upstream:UserInterface/Inscription/RegisterForm.Designer.cs
             this.btnReturnToLogin.BackColor = System.Drawing.Color.LightSalmon;
             this.btnReturnToLogin.FlatAppearance.BorderSize = 0;
             this.btnReturnToLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
@@ -211,13 +152,6 @@ namespace LLWS.UserInterface
             this.btnReturnToLogin.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnReturnToLogin.Name = "btnReturnToLogin";
             this.btnReturnToLogin.Size = new System.Drawing.Size(159, 48);
-=======
-            this.btnReturnToLogin.Font = new System.Drawing.Font("Segoe UI", 9.75F);
-            this.btnReturnToLogin.Location = new System.Drawing.Point(165, 342);
-            this.btnReturnToLogin.Margin = new System.Windows.Forms.Padding(2);
-            this.btnReturnToLogin.Name = "btnReturnToLogin";
-            this.btnReturnToLogin.Size = new System.Drawing.Size(90, 27);
->>>>>>> Stashed changes:UserInterface/RegisterForm.Designer.cs
             this.btnReturnToLogin.TabIndex = 13;
             this.btnReturnToLogin.Text = "Retour";
             this.btnReturnToLogin.UseVisualStyleBackColor = false;
